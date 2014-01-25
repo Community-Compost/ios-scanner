39991a079a23600859661785cdc8b9cab08ca927
Amy Coelho <amy@Amys-MacBook-Air-2.local>
2014-01-25 10:07:18 -0600

    QR code reading setup

--- Community Compost/CCViewController.h
+++ Community Compost/CCViewController.h
@@ -7,7 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface CCViewController : UIViewController
+@interface CCViewController : UIViewController <AVCaptureMetadataOutputObjectsDelegate>
+
+@property (weak, nonatomic) IBOutlet UIView *viewPreview;
+@property (weak, nonatomic) IBOutlet UILabel *lblStatus;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *bbitemStart;
+
+- (IBAction)startStopReading:(id)sender;
 
 @end
